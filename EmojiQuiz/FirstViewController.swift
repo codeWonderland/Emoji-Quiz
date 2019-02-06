d68ae80e9fcb141ceec334226b55732925d0f68e
scaifish <cai.blanchard@mymail.champlain.edu>
2019-02-05 20:43:25 -0500

    Init Commit

---
+++ EmojiQuiz/FirstViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FirstViewController.swift
+//  EmojiQuiz
+//
+//  Created by Blanchard, Cai on 2/5/19.
+//  Copyright © 2019 Blanchard, Cai. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var questionCount: UILabel!
+    @IBOutlet weak var questionStepper: UIStepper!
+    @IBOutlet weak var topicButtonOne: UIButton!
+    @IBOutlet weak var topicButtonTwo: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ EmojiQuiz/ScoreViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  EmojiQuiz
+//
+//  Created by Blanchard, Cai on 2/5/19.
+//  Copyright © 2019 Blanchard, Cai. All rights reserved.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+
+    @IBOutlet weak var topicSelect: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
